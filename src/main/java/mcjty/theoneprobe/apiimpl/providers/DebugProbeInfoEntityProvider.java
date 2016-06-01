1e9858674cab5ccc51867a5f5f6ad0856365eeb3
McJty <mcjty1@gmail.com>
2016-06-01 09:38:23 +0200

    Added debug information for entities (creative probe)

--- src/main/java/mcjty/theoneprobe/apiimpl/providers/DebugProbeInfoEntityProvider.java
+++ src/main/java/mcjty/theoneprobe/apiimpl/providers/DebugProbeInfoEntityProvider.java
@@ -1,11 +1,15 @@
 package mcjty.theoneprobe.apiimpl.providers;
 
+import mcjty.theoneprobe.Config;
 import mcjty.theoneprobe.TheOneProbe;
 import mcjty.theoneprobe.api.IProbeHitEntityData;
 import mcjty.theoneprobe.api.IProbeInfo;
 import mcjty.theoneprobe.api.IProbeInfoEntityProvider;
 import mcjty.theoneprobe.api.ProbeMode;
+import mcjty.theoneprobe.apiimpl.LayoutStyle;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
@@ -18,6 +22,34 @@ public class DebugProbeInfoEntityProvider implements IProbeInfoEntityProvider {
 
     @Override
     public void addProbeEntityInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, Entity entity, IProbeHitEntityData data) {
+        if (mode == ProbeMode.DEBUG && Config.showDebugInfo) {
+            IProbeInfo vertical = null;
+            if (entity instanceof EntityLivingBase) {
+                vertical = probeInfo.vertical(new LayoutStyle().borderColor(0xffff4444).spacing(2));
 
+                EntityLivingBase entityLivingBase = (EntityLivingBase) entity;
+                int totalArmorValue = entityLivingBase.getTotalArmorValue();
+                int age = entityLivingBase.getAge();
+                float absorptionAmount = entityLivingBase.getAbsorptionAmount();
+                float aiMoveSpeed = entityLivingBase.getAIMoveSpeed();
+                int revengeTimer = entityLivingBase.getRevengeTimer();
+                vertical
+                        .text("Tot armor: " + totalArmorValue)
+                        .text("Age: " + age)
+                        .text("Absorption: " + absorptionAmount)
+                        .text("AI Move Speed: " + aiMoveSpeed)
+                        .text("Revenge Timer: " + revengeTimer);
+            }
+            if (entity instanceof EntityAgeable) {
+                if (vertical == null) {
+                    vertical = probeInfo.vertical(new LayoutStyle().borderColor(0xffff4444).spacing(2));
+                }
+
+                EntityAgeable entityAgeable = (EntityAgeable) entity;
+                int growingAge = entityAgeable.getGrowingAge();
+                vertical
+                        .text("Growing Age: " + growingAge);
+            }
+        }
     }
 }
