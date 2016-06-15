f41a38a43d8a0faf7348a8c9291d61f41cc5f215
McJty <mcjty1@gmail.com>
2016-06-15 11:12:51 +0200

    1.0.9

--- src/main/java/mcjty/theoneprobe/TheOneProbe.java
+++ src/main/java/mcjty/theoneprobe/TheOneProbe.java
@@ -26,7 +26,7 @@ import java.io.File;
         version = TheOneProbe.VERSION)
 public class TheOneProbe {
     public static final String MODID = "theoneprobe";
-    public static final String VERSION = "1.0.8";
+    public static final String VERSION = "1.0.9";
     public static final String MIN_FORGE_VER = "12.16.1.1896";
 
     @SidedProxy(clientSide="mcjty.theoneprobe.proxy.ClientProxy", serverSide="mcjty.theoneprobe.proxy.ServerProxy")
