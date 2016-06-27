2bd6dd6dd3ca8bf59b1553e09661d7622193e74d
McJty <mcjty1@gmail.com>
2016-06-27 16:38:09 +0200

    Bumped the minimum forge version

--- src/main/java/mcjty/theoneprobe/TheOneProbe.java
+++ src/main/java/mcjty/theoneprobe/TheOneProbe.java
@@ -27,7 +27,7 @@ import java.io.File;
 public class TheOneProbe {
     public static final String MODID = "theoneprobe";
     public static final String VERSION = "1.0.11";
-    public static final String MIN_FORGE_VER = "12.16.1.1896";
+    public static final String MIN_FORGE_VER = "12.17.0.1976";
 
     @SidedProxy(clientSide="mcjty.theoneprobe.proxy.ClientProxy", serverSide="mcjty.theoneprobe.proxy.ServerProxy")
     public static CommonProxy proxy;
