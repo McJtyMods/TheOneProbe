845b7847cecfa4140bf49e7bfe5b755164bae5ed
McJty <mcjty1@gmail.com>
2016-08-03 09:06:50 +0200

    1.0.15

--- src/main/java/mcjty/theoneprobe/TheOneProbe.java
+++ src/main/java/mcjty/theoneprobe/TheOneProbe.java
@@ -27,7 +27,7 @@ import java.io.File;
         acceptedMinecraftVersions = "[1.9,1.11)")
 public class TheOneProbe {
     public static final String MODID = "theoneprobe";
-    public static final String VERSION = "1.0.14";
+    public static final String VERSION = "1.0.15";
     public static final String MIN_FORGE_VER = "12.17.0.1976";
 
     @SidedProxy(clientSide="mcjty.theoneprobe.proxy.ClientProxy", serverSide="mcjty.theoneprobe.proxy.ServerProxy")
