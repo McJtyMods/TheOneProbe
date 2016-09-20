1e5e12cf6dbc89578793a0c72571b47ceff9d2f7
McJty <mcjty1@gmail.com>
2016-09-20 09:25:47 +0200

    1.1.2

--- src/main/java/mcjty/theoneprobe/TheOneProbe.java
+++ src/main/java/mcjty/theoneprobe/TheOneProbe.java
@@ -28,7 +28,7 @@ import java.io.File;
         guiFactory = "mcjty.theoneprobe.config.TopModGuiFactory")
 public class TheOneProbe {
     public static final String MODID = "theoneprobe";
-    public static final String VERSION = "1.1.1a";
+    public static final String VERSION = "1.1.2";
     public static final String MIN_FORGE_VER = "12.17.0.1976";
 
     @SidedProxy(clientSide="mcjty.theoneprobe.proxy.ClientProxy", serverSide="mcjty.theoneprobe.proxy.ServerProxy")
