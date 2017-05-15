7fbc4df185338fb0478e852cb158e896b3ff9dce
McJty <mcjty1@gmail.com>
2017-05-15 11:36:50 +0200

    Also show the classname of the block with the creative probe

--- src/main/java/mcjty/theoneprobe/apiimpl/providers/DebugProbeInfoProvider.java
+++ src/main/java/mcjty/theoneprobe/apiimpl/providers/DebugProbeInfoProvider.java
@@ -35,10 +35,12 @@ public class DebugProbeInfoProvider implements IProbeInfoProvider {
     }
 
     private void showDebugInfo(IProbeInfo probeInfo, World world, IBlockState blockState, BlockPos pos, Block block, EnumFacing side) {
+        String simpleName = block.getClass().getSimpleName();
         IProbeInfo vertical = probeInfo.vertical(new LayoutStyle().borderColor(0xffff4444).spacing(2))
                 .text(LABEL + "Reg Name: " + INFO + block.getRegistryName().toString())
                 .text(LABEL + "Unlocname: " + INFO + block.getUnlocalizedName())
                 .text(LABEL + "Meta: " + INFO + blockState.getBlock().getMetaFromState(blockState))
+                .text(LABEL + "Class: " + INFO + simpleName)
                 .text(LABEL + "Hardness: " + INFO + block.getBlockHardness(blockState, world, pos))
                 .text(LABEL + "Power W: " + INFO + block.getWeakPower(blockState, world, pos, side.getOpposite())
                         + LABEL + ", S: " + INFO + block.getStrongPower(blockState, world, pos, side.getOpposite()))
