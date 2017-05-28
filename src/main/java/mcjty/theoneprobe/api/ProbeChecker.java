c11b292ff9a51c92f2dc987f42d83e2041de3b28
gt22 <igorengel@mail.ru>
2017-05-28 17:37:19 +0300

    A way to add custom checks

--- src/main/java/mcjty/theoneprobe/api/ProbeChecker.java
+++ src/main/java/mcjty/theoneprobe/api/ProbeChecker.java
@@ -2,15 +2,27 @@ package mcjty.theoneprobe.api;
 
 import baubles.api.BaublesApi;
 import baubles.api.cap.IBaublesItemHandler;
+import com.google.common.collect.Sets;
 import mcjty.theoneprobe.TheOneProbe;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 
+import java.util.Set;
+
 public class ProbeChecker {
 
 	public static String PROBETAG = "theoneprobe";
 	public static String PROBETAG_HAND = "theoneprobe_hand";
+	private static final Set<IProbeCheck> CHECKS = Sets.newHashSet();
+
+	//Default checks
+	static {
+		addCheck(p -> hasProbeInHand(p, EnumHand.MAIN_HAND));
+		addCheck(p -> hasProbeInHand(p, EnumHand.OFF_HAND));
+		addCheck(ProbeChecker::hasProbeInHelmet);
+		addCheck(ProbeChecker::hasProbeInBauble);
+	}
 
 	public static boolean canWorkAsProbe(ItemStack stack, EntityPlayer player, String nbtProbeTagName) {
 		if (stack == null) {
@@ -23,8 +35,7 @@ public class ProbeChecker {
 	}
 
 	public static boolean hasAProbeSomewhere(EntityPlayer player) {
-		return hasProbeInHand(player, EnumHand.MAIN_HAND) || hasProbeInHand(player, EnumHand.OFF_HAND) || hasProbeInHelmet(player)
-			|| hasProbeInBauble(player);
+		return CHECKS.parallelStream().anyMatch(c -> c.hasProbe(player));
 	}
 
 	public static boolean hasProbeInHand(EntityPlayer player, EnumHand hand) {
@@ -46,6 +57,10 @@ public class ProbeChecker {
 			}
 		}
 		return false;
+	}
+
+	public static void addCheck(IProbeCheck check) {
+		CHECKS.add(check);
 	}
 
 }
