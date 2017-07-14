6d46fc242398eb01e606efce31d8f72cbc1572ae
McJty <mcjty1@gmail.com>
2017-07-14 08:03:07 +0200

    Fixed in-game MOD config GUI

--- src/main/java/mcjty/theoneprobe/config/TopModGuiFactory.java
+++ src/main/java/mcjty/theoneprobe/config/TopModGuiFactory.java
@@ -15,12 +15,12 @@ public class TopModGuiFactory implements IModGuiFactory {
 
     @Override
     public boolean hasConfigGui() {
-        return false;
+        return true;
     }
 
     @Override
     public GuiScreen createConfigGui(GuiScreen parentScreen) {
-        return null;
+        return new TopModConfigGui(parentScreen);
     }
 
     @Override
