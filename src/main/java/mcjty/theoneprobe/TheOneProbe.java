eed44ff95cb19da9469dc86cf37768561ae91d92
McJty <mcjty1@gmail.com>
2019-07-23 05:23:19 +0200

    Version bumps

--- src/main/java/mcjty/theoneprobe/TheOneProbe.java
+++ src/main/java/mcjty/theoneprobe/TheOneProbe.java
@@ -49,8 +49,6 @@ import java.util.function.Supplier;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TheOneProbe {
     public static final String MODID = "theoneprobe";
-    public static final String VERSION = "1.4.30";
-    public static final String MIN_FORGE11_VER = "13.19.0.2176";
 
     public static IProxy proxy = DistExecutor.runForDist(() -> () -> new ClientProxy(), () -> () -> new ServerProxy());
     public static final Logger logger = LogManager.getLogger();
