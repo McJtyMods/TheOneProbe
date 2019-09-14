446d7565958935939bbbdfc93c4d8ecabde35169
McJty <slto15aGH10>
2019-09-14 05:41:21 +0200

    Experiments with data generators

---
+++ src/main/java/mcjty/theoneprobe/datagen/LootTables.java
@@ -0,0 +1,16 @@
+package mcjty.theoneprobe.datagen;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DirectoryCache;
+import net.minecraft.data.LootTableProvider;
+
+public class LootTables extends LootTableProvider {
+
+    public LootTables(DataGenerator dataGeneratorIn) {
+        super(dataGeneratorIn);
+    }
+
+    @Override
+    public void act(DirectoryCache cache) {
+    }
+}
