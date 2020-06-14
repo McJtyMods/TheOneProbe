401672e5f85a2ef0d670db30f043f42ca0507912
McJty <slto15aGH10>
2020-06-14 17:23:16 +0200

    Small teak

--- src/main/java/mcjty/theoneprobe/api/CompoundText.java
+++ src/main/java/mcjty/theoneprobe/api/CompoundText.java
@@ -11,13 +11,20 @@ public class CompoundText {
         return new CompoundText();
     }
 
+    /// Use this to set a style that the player can configure client-side. This is the recommended way to do text formatting
     public CompoundText style(TextStyleClass style) {
         ITextComponent cmp = new StringTextComponent(style.toString());
         return newComponent(cmp);
     }
 
+    /// The prefered way to do translatable text
     public CompoundText text(ITextComponent cmp) {
         return newComponent(cmp);
+    }
+
+    /// Only use this for small strings or numbers for which no translation is useful
+    public CompoundText text(String text) {
+        return newComponent(new StringTextComponent(text));
     }
 
     private CompoundText newComponent(ITextComponent cmp) {
