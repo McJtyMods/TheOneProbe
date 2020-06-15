433ed605cf67f11b168a07d5e519fe2fc42cb89c
McJty <mcjty1@gmail.com>
2020-06-15 05:04:28 +0200

    2.0.4

--- src/main/java/mcjty/theoneprobe/api/CompoundText.java
+++ src/main/java/mcjty/theoneprobe/api/CompoundText.java
@@ -2,6 +2,7 @@ package mcjty.theoneprobe.api;
 
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class CompoundText {
 
@@ -25,6 +26,46 @@ public class CompoundText {
     /// Only use this for small strings or numbers for which no translation is useful
     public CompoundText text(String text) {
         return newComponent(new StringTextComponent(text));
+    }
+
+    /// Shorthand for style(TextStyleClass.INFO).text(new TranslationTextComponent(translationKey):
+    public CompoundText info(String translationKey) {
+        return style(TextStyleClass.INFO).text(new TranslationTextComponent(translationKey));
+    }
+
+    /// Shorthand for style(TextStyleClass.INFOIMP).text(new TranslationTextComponent(translationKey):
+    public CompoundText important(String translationKey) {
+        return style(TextStyleClass.INFOIMP).text(new TranslationTextComponent(translationKey));
+    }
+
+    /// Shorthand for style(TextStyleClass.WARNING).text(new TranslationTextComponent(translationKey):
+    public CompoundText warning(String translationKey) {
+        return style(TextStyleClass.WARNING).text(new TranslationTextComponent(translationKey));
+    }
+
+    /// Shorthand for style(TextStyleClass.ERROR).text(new TranslationTextComponent(translationKey):
+    public CompoundText error(String translationKey) {
+        return style(TextStyleClass.ERROR).text(new TranslationTextComponent(translationKey));
+    }
+
+    /// Shorthand for style(TextStyleClass.LABEL).text(new TranslationTextComponent(translationKey):
+    public CompoundText label(String translationKey) {
+        return style(TextStyleClass.LABEL).text(new TranslationTextComponent(translationKey));
+    }
+
+    /// Shorthand for style(TextStyleClass.OK).text(new TranslationTextComponent(translationKey):
+    public CompoundText ok(String translationKey) {
+        return style(TextStyleClass.OK).text(new TranslationTextComponent(translationKey));
+    }
+
+    /// Shorthand for style(TextStyleClass.NAME).text(new TranslationTextComponent(translationKey):
+    public CompoundText name(String translationKey) {
+        return style(TextStyleClass.NAME).text(new TranslationTextComponent(translationKey));
+    }
+
+    /// Shorthand for style(TextStyleClass.PROGRESS).text(new TranslationTextComponent(translationKey):
+    public CompoundText progress(String translationKey) {
+        return style(TextStyleClass.PROGRESS).text(new TranslationTextComponent(translationKey));
     }
 
     private CompoundText newComponent(ITextComponent cmp) {
