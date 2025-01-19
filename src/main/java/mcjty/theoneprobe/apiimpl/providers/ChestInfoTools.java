1634952d5897b8c64364f650a9ed7699563598e1
McJty <mcjty1@gmail.com>
2025-01-19 15:52:08 +0100

    TOP will now no longer try to resolve the contents of a lootbox. Instead it will show that the loot hasn't been resolved yet. With the debug probe you can now also see the name of the loot table

--- src/main/java/mcjty/theoneprobe/apiimpl/providers/ChestInfoTools.java
+++ src/main/java/mcjty/theoneprobe/apiimpl/providers/ChestInfoTools.java
@@ -12,9 +12,9 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.items.IItemHandler;
-import net.neoforged.neoforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
@@ -26,6 +26,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ChestInfoTools {
 
     static void showChestInfo(ProbeMode mode, IProbeInfo probeInfo, Level world, BlockPos pos, IProbeConfig config) {
+        if (world.getBlockEntity(pos) instanceof RandomizableContainerBlockEntity randomizable) {
+            if (randomizable.lootTable != null) {
+                // Loottable hasn't been resolved yet
+                if (mode == ProbeMode.DEBUG) {
+                    probeInfo.horizontal().text("Loot table: " + randomizable.lootTable.toString());
+                } else {
+                    probeInfo.horizontal().text(CompoundText.create().style(TextStyleClass.WARNING).text("Loot hasn't resolved yet!"));
+                }
+                return;
+            }
+        }
+
         List<ItemStack> stacks = null;
         IProbeConfig.ConfigMode chestMode = config.getShowChestContents();
         if (chestMode == IProbeConfig.ConfigMode.EXTENDED && (Config.showSmallChestContentsWithoutSneaking.get() > 0 || !Config.getInventoriesToShow().isEmpty())) {
